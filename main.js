846a92650a5fd6add924199ecdd133706ea89f64
oliverlo1 <113052541+oliverlo1@users.noreply.github.com>
2022-11-09 10:47:38 -0700

    Update main.js

--- main.js
+++ main.js
@@ -15,32 +15,62 @@ function mousemoveHandler(event) {
     // Calc mouse coordinates using mouse event and canvas location info
     mouseX = Math.round(event.clientX - cnvRect.left);
     mouseY = Math.round(event.clientY - cnvRect.top);
+    // console.log(mouseX, mouseY);
 }
 
 let rectX = 200;
-let rectY = 150;
-let circX = 150;
-let circY = 150;
-
-if (rectX <= mouseX <= rectX + 100 && rectY <= mouseY <= rectY + 60) {
-    rectX = Math.random();
-    rectY = Math.random();
-}
+let rectY = 270;
+let r1 = 255;
+let g1 = 255;
+let b1 = 255;
+let r2 = 255;
+let g2 = 255;
+let b2 = 255;
+let circX = 550;
+let circY = 300;
 
 requestAnimationFrame(animate);
 function animate() {
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, cnv.width, cnv.height);
 
+    if (rectX <= mouseX && mouseX <= rectX + 100 && rectY <= mouseY && mouseY <= rectY + 60) {
+        rectX = Math.random() * 700;
+        rectY = Math.random() * 540;
+        r1 = Math.trunc(Math.random() * 255);
+        g1 = Math.trunc(Math.random() * 255);
+        b1 = Math.trunc(Math.random() * 255);
+    }
+    
+    if (circX - 50 <= mouseX && mouseX <= circX + 50 && circY - 50 <= mouseY && mouseY <= circY + 50) {
+        circX = (Math.random() * 700) + 50;
+        circY = (Math.random() * 500) + 50;
+        r2 = Math.trunc(Math.random() * 255);
+        g2 = Math.trunc(Math.random() * 255);
+        b2 = Math.trunc(Math.random() * 255);
+        console.log(r2);
+    }
 
-    ctx.beginPath
-    ctx.strokeStyle = "rgb(255, 255, 255)";
+    if (r1 <= 0 && g1 <= 0 && b1 <= 0) {
+        r1 = 255;
+        g1 = 255;
+        b1 = 255;
+    }
+
+    if (r2 <= 0 && g2 <= 0 && b2 <= 0) {
+        r2 = 255;
+        g2 = 255;
+        b2 = 255;
+    }
+
+    ctx.beginPath;
+    ctx.strokeStyle = "rgb(" + r1 + ", " + g1 + ", " + b1 + ")";
     ctx.lineWidth = 4;
     ctx.strokeRect(rectX, rectY, 100, 60);
 
-    ctx.strokeStyle = "(255, 255, 255)";
+    ctx.strokeStyle = "rgb(2" + r2 + ", " + g2 + ", " + b2 + ")";
     ctx.beginPath();
-    ctx.arc(circX, circY, 100, 0, 2 * Math.PI);
+    ctx.arc(circX, circY, 50, 0, 2 * Math.PI);
     ctx.stroke();
 
     requestAnimationFrame(animate)
